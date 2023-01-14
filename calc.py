665347008d8a6ed6db2fadef098fc37539cb1213
Spchavez13 <roberto.chavez@gmail.com>
2023-01-14 13:44:24 -0600

    Se agregan todas la operaciones matematicas

--- calc.py
+++ calc.py
@@ -10,8 +10,39 @@ def menu():
     print("7. Seno")
     print("8. Coseno")
     print("9. Tangente")
-    return int(input("Seleccione una opción: "))
+    return int(input("Seleccione una opción de la operacion que dese realiar: "))
+
+#"Se agregan todas las opciones matematicas"
+
+def sumar(a, b):
+    return a + b
+ 
+def restar(a, b):
+    return a - b
+ 
+def multiplicar(a, b):
+    return a * b
+ 
+def dividir(a, b):
+    return a / b
+ 
+def raiz_n(a, b):
+    return a ** (1/b)
+ 
+def exponente_n(a, b):
+    return a ** b
+ 
+def seno(a):
+    return math.sin(a)
+ 
+def coseno(a):
+    return math.cos(a)
+ 
+def tangente(a):
+    return math.tan(a)
+ 
 
 opcion = menu()
 a= float(input("ingrese el primer numero: "))
 b= float(input("Ingrese un segundo nundo numero:  "))
+
